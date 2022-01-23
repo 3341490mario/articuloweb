1d47faff17efda6dff71f0cbba269ceeba440b6a
3341490mario <3341490@alu.murciaeduca.es>
2022-01-23 14:45:08 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Cabecera />
+</template>
+
+<script setup>
+import Cabecera from "./components/Cabecera.vue";
+</script>
+
+<style>
+body {
+  margin: 0px;
+  padding: 0px;
+}
+</style>
---
+++ src/components/Cabecera.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="container">
+    <div id="cabecera">
+      <img src="#" alt="logo" />
+      <nav class="menu">
+        <ul>
+          <li><a href="">Menu 1</a></li>
+          <li><a href="">Menu 2</a></li>
+          <li><a href="">Menu 3</a></li>
+          <li><a href="">Menu 4</a></li>
+          <li><a href="">Menu 5</a></li>
+        </ul>
+      </nav>
+      <div class="session">
+        <a href="#" class="button">Registrarse</a>
+        <a href="#" class="button">Iniciar sesión</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style>
+#cabecera {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: lightblue;
+}
+
+#cabecera .menu {
+  background-color: lightyellow;
+  display: block;
+}
+
+#cabecera .menu ul {
+  display: flex;
+  list-style: none;
+  padding: 1rem;
+}
+
+.session {
+  display: flex;
+  background-color: lime;
+}
+
+.button {
+  padding: 1rem;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
